948d6f411391793553f21967eeaef79d87299e7e
Suwas52 <suwasdanuwar18@gmail.com>
2023-02-14 08:30:51 +0545

    initial commit

---
+++ Subash.java
@@ -0,0 +1,14 @@
+public class Subash{
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Katari Rider k7");
+        System.out.println("");
+        System.out.println("Address:");
+        System.out.println("----State: Sagarmatha 01");
+        System.out.println("----Zip Code: 525");
+        System.out.println("----Country: Nepal");
+        System.out.println("");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9808082356");
+        System.out.println("----Email Address: suwasdanuwar@gmail.com");
+    }
+}
